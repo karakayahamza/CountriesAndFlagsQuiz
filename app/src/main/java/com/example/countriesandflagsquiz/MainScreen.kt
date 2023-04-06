ced87b87dd1688efbfce5d100dc9ad5c0a2a888b
Krky1418 <190316021@ogr.cbu.edu.tr>
2023-04-07 02:07:51 +0300

    UI

--- app/src/main/java/com/example/countriesandflagsquiz/MainScreen.kt
+++ app/src/main/java/com/example/countriesandflagsquiz/MainScreen.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 class MainScreen : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,44 @@ class MainScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val data = Data.Builder().putInt("intKey",1).build()
+
+        val constraints = Constraints.Builder()
+            //.setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(false)
+            .build()
+
+        val myWorkRequest : PeriodicWorkRequest = PeriodicWorkRequestBuilder<WorkManagerRefreshData>(15,TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
+
+        WorkManager.getInstance(requireContext()).enqueue(myWorkRequest)
+
+        WorkManager.getInstance(requireContext()).getWorkInfoByIdLiveData(myWorkRequest.id).observe(viewLifecycleOwner,
+            Observer {
+                if (it.state == WorkInfo.State.RUNNING){
+                    println("Running...")
+                }
+                else if (it.state == WorkInfo.State.FAILED){
+                    println("Failed...")
+                }
+                else if (it.state == WorkInfo.State.SUCCEEDED){
+                    println("Succeesed.")
+                }
+            })
+
+        //WorkManager.getInstance(this.requireContext()).cancelAllWork()
+
+        //Chaining
+        val oneTimeWorkRequest : OneTimeWorkRequest = OneTimeWorkRequestBuilder<WorkManagerRefreshData>()
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
+        WorkManager.getInstance(requireContext()).beginWith(oneTimeWorkRequest)
+            .then(oneTimeWorkRequest)
+            .then(oneTimeWorkRequest)
+            .enqueue()
         return inflater.inflate(R.layout.fragment_main_screen, container, false)
     }
-
 }
