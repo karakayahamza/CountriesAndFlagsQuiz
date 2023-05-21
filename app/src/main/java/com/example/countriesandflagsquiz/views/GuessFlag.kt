4e5a4ca89d966a698cf3b1bb0a271fb621cb9842
hkkrk <hamza.karakaya77@outlook.com>
2023-05-21 23:17:46 +0300

    FirstCommit

--- app/src/main/java/com/example/countriesandflagsquiz/views/GuessFlag.kt
+++ app/src/main/java/com/example/countriesandflagsquiz/views/GuessFlag.kt
@@ -3,40 +3,42 @@ package com.example.countriesandflagsquiz.views
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
+import android.app.ProgressDialog
 import android.net.Uri
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.example.countriesandflagsquiz.R
 import com.example.countriesandflagsquiz.databinding.FragmentGuessFlagBinding
+import com.example.countriesandflagsquiz.models.CountriesFlagsModel
 import com.example.countriesandflagsquiz.randomFlags
 import com.example.countriesandflagsquiz.viewmodels.CountriesAndFlagsViewModel
 import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
-import kotlin.concurrent.timer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class GuessFlag : Fragment() {
     private var _binding: FragmentGuessFlagBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: CountriesAndFlagsViewModel
-    var score = 0
-    var condition = false
+    private var score = 0
+    private var condition = false
     lateinit var countDownTimer : CountDownTimer
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var model : CountriesFlagsModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,12 +46,27 @@ class GuessFlag : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentGuessFlagBinding.inflate(inflater, container, false)
+
+        viewModel = ViewModelProviders.of(this)[CountriesAndFlagsViewModel::class.java]
+        viewModel.loadData()
+
+        val progressDialog = ProgressDialog.show(requireContext(), "Bekleyin", "İndiriliyor...", true)
+        viewModel.countriesAndFlags.observe(viewLifecycleOwner) { counrty ->
+            counrty.let {
+                model = it!!
+            }
+            progressDialog.dismiss()
+            startTimer()
+            loadNewQuestion()
+        }
+
+
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val dialog = Dialog(requireContext())
+        /*val dialog = Dialog(requireContext())
         dialog.setContentView(R.layout.popup_layout)
         val okButton = dialog.findViewById<Button>(R.id.ok_button)
         okButton.setOnClickListener {
@@ -58,11 +75,10 @@ class GuessFlag : Fragment() {
             loadNewQuestion()
             dialog.dismiss()
         }
-        dialog.show()
-        viewModel = ViewModelProviders.of(this)[CountriesAndFlagsViewModel::class.java]
-        viewModel.loadData()
+        dialog.show()*/
     }
-    fun setUpListener(coutryName :String){
+    @SuppressLint("ResourceAsColor")
+    private fun setUpListener(coutryName :String){
         if (coutryName==""){
             binding.aOption.isClickable = false
             binding.bOption.isClickable = false
@@ -75,17 +91,35 @@ class GuessFlag : Fragment() {
             binding.cOption.isClickable = true
             binding.dOption.isClickable = true
         }
+
+
         binding.aOption.setOnClickListener {
-            checkAnswer(binding.aOption.text.toString(),coutryName)
+            if (checkAnswer(binding.aOption.text.toString(),coutryName)){
+                binding.aOption.setBackgroundResource(R.drawable.correct_answer)
+            }
+            else binding.aOption.setBackgroundResource(R.drawable.wrong_answer)
+           // binding.aOption.setBackgroundResource(R.drawable.buttun_design)
         }
         binding.bOption.setOnClickListener {
-            checkAnswer(binding.bOption.text.toString(),coutryName)
+            if (checkAnswer(binding.bOption.text.toString(),coutryName)){
+                binding.bOption.setBackgroundResource(R.drawable.correct_answer)
+            }
+            else binding.bOption.setBackgroundResource(R.drawable.wrong_answer)
+           // binding.bOption.setBackgroundResource(R.drawable.buttun_design)
         }
         binding.cOption.setOnClickListener {
-            checkAnswer(binding.cOption.text.toString(),coutryName)
+            if (checkAnswer(binding.cOption.text.toString(),coutryName)){
+                binding.cOption.setBackgroundResource(R.drawable.correct_answer)
+            }
+            else binding.cOption.setBackgroundResource(R.drawable.wrong_answer)
+            //binding.cOption.setBackgroundResource(R.drawable.buttun_design)
         }
         binding.dOption.setOnClickListener {
-            checkAnswer(binding.dOption.text.toString(),coutryName)
+            if (checkAnswer(binding.dOption.text.toString(),coutryName)){
+                binding.dOption.setBackgroundResource(R.drawable.correct_answer)
+            }
+            else binding.dOption.setBackgroundResource(R.drawable.wrong_answer)
+            //binding.dOption.setBackgroundResource(R.drawable.buttun_design)
         }
     }
 
@@ -93,19 +127,30 @@ class GuessFlag : Fragment() {
     fun checkAnswer(answer : String, coutryName:String):Boolean{
 
         if (answer==coutryName){
-            Toast.makeText(requireContext(),"Answer Correct",Toast.LENGTH_SHORT).show()
             score += 1
             condition = true
             binding.questNo.text = "Correct Answers: $score"
-            loadNewQuestion()
+
+            countDownTimer.cancel()
+            val alert = AlertDialog.Builder(requireContext())
+            alert.setTitle("Answer correct")
+            alert.setPositiveButton("Next") { _, _ ->
+                binding.aOption.setBackgroundResource(R.drawable.buttun_design)
+                binding.bOption.setBackgroundResource(R.drawable.buttun_design)
+                binding.cOption.setBackgroundResource(R.drawable.buttun_design)
+                binding.dOption.setBackgroundResource(R.drawable.buttun_design)
+                loadNewQuestion()
+            }
+
+
+            alert.show()
         }
         else{
-            Toast.makeText(requireContext(),"Answer Wrong",Toast.LENGTH_SHORT).show()
             condition = false
             binding.questNo.text = "Correct Answers: $score"
 
             val alert = AlertDialog.Builder(requireContext())
-            alert.setTitle("Game Over \n Score: $score")
+            alert.setTitle("Correct Answer: $coutryName \n Score: $score \n Game Over")
             alert.setMessage("Try Again?")
             alert.setPositiveButton("Yes") { _, _ ->
                 score = 0
@@ -124,13 +169,10 @@ class GuessFlag : Fragment() {
     }
 
     fun startTimer(){
-        condition = true // Flag to track wrong answer
-        countDownTimer = object : CountDownTimer(15500, 1000) {
+        countDownTimer = object : CountDownTimer(5000, 1000) {
             override fun onFinish() {
-               // handler.removeCallbacks(runnable)
-
                 val alert = AlertDialog.Builder(requireContext())
-                alert.setTitle("Wrong Answer")
+                alert.setTitle("Time is over")
                 alert.setMessage("Try Again?")
                 alert.setPositiveButton("Yes") { _, _ ->
                     startTimer()
@@ -138,41 +180,36 @@ class GuessFlag : Fragment() {
                 }
                 alert.setNegativeButton("No") { _, _ ->
                     Toast.makeText(requireContext(), "Game Over", Toast.LENGTH_LONG).show()
-                    alert.show()
                     val action = GuessFlagDirections.actionGuessFlagToMainScreen()
                     Navigation.findNavController(binding.root).navigate(action)
-                    }
+                }
+                alert.show()
                 countDownTimer.cancel()
+                Toast.makeText(requireContext(),"Time is Over",Toast.LENGTH_SHORT).show()
             }
+
             @SuppressLint("SetTextI18n")
             override fun onTick(millisUntilFinished: Long) {
                 binding.countdown.text = (millisUntilFinished / 1000).toString()
             }
         }.start()
-
-        condition = false
-        if (condition){
-            countDownTimer.cancel()
-        }
     }
 
     fun loadNewQuestion(){
         var coutryName= ""
+            val countries = randomFlags()
 
-        viewModel.countriesAndFlags.observe(viewLifecycleOwner) { counrty ->
-            counrty.let {
-                val countries = randomFlags()
+            binding.aOption.text = model.data[countries.elementAtOrNull(0)!!].name.toString()
+            binding.bOption.text = model.data[countries.elementAtOrNull(1)!!].name.toString()
+            binding.cOption.text = model.data[countries.elementAtOrNull(2)!!].name.toString()
+            binding.dOption.text = model.data[countries.elementAtOrNull(3)!!].name.toString()
 
-                binding.aOption.text = counrty!!.data[countries.elementAtOrNull(0)!!].name.toString()
-                binding.bOption.text = counrty.data[countries.elementAtOrNull(1)!!].name.toString()
-                binding.cOption.text = counrty.data[countries.elementAtOrNull(2)!!].name.toString()
-                binding.dOption.text = counrty.data[countries.elementAtOrNull(3)!!].name.toString()
+            val choseRandomAnswer = model.data[countries.random()]
+            GlideToVectorYou.justLoadImage(requireActivity(), Uri.parse(choseRandomAnswer.flag.toString()), binding.flagImage)
+            coutryName= choseRandomAnswer.name.toString()
 
-                val choseRandomAnswer = counrty.data[countries.random()]
-                GlideToVectorYou.justLoadImage(requireActivity(), Uri.parse(choseRandomAnswer.flag.toString()), binding.flagImage)
-                coutryName= choseRandomAnswer.name.toString()
-            }
+            countDownTimer.cancel()
+            countDownTimer.start()
             setUpListener(coutryName)
         }
     }
-}
